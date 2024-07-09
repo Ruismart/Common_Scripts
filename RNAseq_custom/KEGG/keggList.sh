0a8d81e7c19016049fb1ff3a766142af0bd5dc05
Ruismart <liushaorui@mail.bnu.edu.cn>
2024-07-09 16:41:57 +0800

    update custom KEGG scripts

--- RNAseq_custom/KEGG/keggList.sh
+++ RNAseq_custom/KEGG/keggList.sh
@@ -2,6 +2,7 @@
 # This is a Script for getting KEGG Organism code list
 # Liu, Shaorui; liushaorui@mail.bnu.edu.cn; 2018-1-24
 
-Rscript /home/shaorui/Install/KEGG/keggList.R
+#Rscript /home/shaorui/Install/KEGG/keggList.R
+Rscript $(which keggList.R)
 cut -f 2,3 keggList.xls > keggList
 rm keggList.xls 
